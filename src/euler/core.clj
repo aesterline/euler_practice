00fa4fa19e7e13e56bbeef92aaa8c590452ed01a
Adam Esterline <adam@esterlines.com>
2010-11-11 21:01:46 -0700

    sum of even fibs less than 4000000

--- src/euler/core.clj
+++ src/euler/core.clj
@@ -1,4 +1,6 @@
-(ns euler.core)
+(ns euler.core
+  (:require [clojure.contrib.lazy-seqs :as lazy]
+            [clojure.contrib.math :as math]))
 
 (defn divides? [dividend divisor]
   (zero? (rem dividend divisor)))
@@ -14,3 +16,13 @@
      (range max)
      (filter (divides-any 3 5))
      (reduce +))))
+
+(defn even-fib-sum-less-than
+  ([] (even-fib-sum-less-than 4000000))
+  ([max]
+   (->>
+     (take-while #(< % max) (lazy/fibs))
+     (filter even?)
+     (reduce +))))
+
+  
