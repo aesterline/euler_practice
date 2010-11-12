a8cfa5e1029cda822e555349ed3732eaa9ef2941
Adam Esterline <adam@esterlines.com>
2010-11-11 17:03:40 -0700

    Initial commit

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject euler "1.0.0-SNAPSHOT"
+  :description "Project Euler Solutions"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
